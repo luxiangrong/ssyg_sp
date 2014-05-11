9c99c79808e964b547cae3fb5b83d07a4acac238
luxiangrong <luxiangrongwave@gmail.com>
2014-05-11 18:44:38 +0800

    为左侧菜单添加滑动显示效果

---
+++ script/jquery.touchwipe.js
@@ -0,0 +1,111 @@
+/**
+ * jQuery Plugin to obtain touch gestures from iPhone, iPod Touch and iPad, should also work with Android mobile phones (not tested yet!)
+ * Common usage: wipe images (left and right to show the previous or next image)
+ *
+ * @author Andreas Waltl, netCU Internetagentur (http://www.netcu.de)
+ * @version 1.1.1 (9th December 2010) - fix bug (older IE's had problems)
+ * @version 1.1 (1st September 2010) - support wipe up and wipe down
+ * @version 1.0 (15th July 2010)
+ */
+(function($) {
+	$.fn.touchwipe = function(settings) {
+		var config = {
+			min_move_x : 20,
+			min_move_y : 20,
+			wipeLeft : function() {
+			},
+			wipeRight : function() {
+			},
+			wipeUp : function() {
+			},
+			wipeDown : function() {
+			},
+			wipeMoving: function(){
+			},
+			preventDefaultEvents : true,
+			stopPropagation : true
+		};
+
+		if (settings)
+			$.extend(config, settings);
+
+		this.each(function() {
+			var startX;
+			var startY;
+			var dx;
+			var dy;
+			var isMoving = false;
+
+			function cancelTouch() {
+				this.removeEventListener('touchmove', onTouchMove);
+				startX = null;
+				startY = null;
+				dx = null;
+				dy = null;
+				isMoving = false;
+			}
+
+			function onTouchMove(e) {
+				if (config.preventDefaultEvents) {
+					e.preventDefault();
+				}
+				if (config.stopPropagation) {
+					$.Event(e).stopPropagation();
+				}
+				if (isMoving) {
+					var x = e.touches[0].pageX;
+					var y = e.touches[0].pageY;
+					 dx = startX - x;
+					 dy = startY - y;
+					config.wipeMoving(dx, dy);
+				}
+			}
+
+			function onTouchEnd(e) {
+				if (isMoving) {
+					if (Math.abs(dx) >= config.min_move_x) {
+						if (dx > 0) {
+							config.wipeLeft();
+						} else {
+							config.wipeRight();
+						}
+						cancelTouch();
+					} else if (Math.abs(dy) >= config.min_move_y) {
+						if (dy > 0) {
+							config.wipeDown();
+						} else {
+							config.wipeUp();
+						}
+						cancelTouch();
+					}
+				}
+			}
+
+			function onTouchStart(e) {
+				if (config.stopPropagation) {
+					$.Event(e).stopPropagation();
+				}
+				if(e.target) {
+					var $target = $(e.target);
+					if($target.attr('data-stopPropagation')) {
+						return;
+					}
+				}
+				if (e.touches.length == 1) {
+					startX = e.touches[0].pageX;
+					startY = e.touches[0].pageY;
+					isMoving = true;
+					this.addEventListener('touchmove', onTouchMove, false);
+					this.addEventListener('touchend', onTouchEnd, true);
+				}
+			}
+
+			if ('ontouchstart' in document.documentElement) {
+				this.addEventListener('touchstart', onTouchStart, false);
+			}
+		});
+
+		return this;
+	};
+
+})(jQuery);
--- script/js.js
+++ script/js.js
@@ -1,13 +1,91 @@
-$(function(){
+$(function() {
 	//--左侧菜单
-	$('.headDiv').find('a').eq(0).click(function(){
-		$('.pageWal').addClass('pageWalNow');
-		$('.sideNav').addClass('sideNavNow');
-		})
-	$('.sideNav').find('.back').click(function(){
-		$('.pageWal').removeClass('pageWalNow');
-		$('.sideNav').removeClass('sideNavNow');
-		})
-	//--焦点图	
+	$('.headDiv').find('a').eq(0).click(function() {
+		showSideNav();
+	});
+	$('.sideNav').find('.back').click(function() {
+		hideSideNav();
+	});
+	
+	var winWidth = $(window).width();
+	function showSideNav() {
+		$(".sideNav").removeClass('no-transition');
+		$(".pageWal").removeClass('no-transition');
+		
+		$(".sideNav").css('box-shadow',  '3px 3px 5px #666666');
+		
+		$(".sideNav").css('transform', 'translate3d(0px, 0px, 0px)');
+		$(".sideNav").css('-webkit-transform', 'translate3d(0px, 0px, 0px)');
+		
+		$(".pageWal").css('transform', 'translate3d(' + $(".sideNav").width()  + 'px, 0px, 0px)');
+		$(".pageWal").css('-webkit-transform', 'translate3d(' + $(".sideNav").width()  + 'px, 0px, 0px)');
+	}
+	
+	function hideSideNav() {
+		
+		$(".sideNav").removeClass('no-transition');
+		$(".pageWal").removeClass('no-transition');
+		
+		$(".sideNav").css('box-shadow',  'none');
+		
+		$(".sideNav").css('transform', 'translate3d(-' + $(".sideNav").width() + 'px, 0px, 0px)');
+		$(".sideNav").css('-webkit-transform', 'translate3d(-' + $(".sideNav").width() + 'px, 0px, 0px)');
+		
+		$(".pageWal").css('transform', 'translate3d(0px, 0px, 0px)');
+		$(".pageWal").css('-webkit-transform', 'translate3d(0px, 0px, 0px)');
+	}
+	
+	var parseToMatrix = function(str) {
+		var reg = /^matrix\((-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)\)$/;
+		var matches = str.match(reg);
+		if($.isArray( matches) && matches.length == 7) {
+			matches.splice(0,1);
+			return matches;
+		}
+		return [0,0,0,0,0,0];
+	};
+	
+	var initTranslate3dX = {
+		'sideNav' : 0,
+		'pageWal' : 0
+	};
+	$("body").on('touchstart', function(){
+		initTranslate3dX ={
+			'sideNav' : parseToMatrix($(".sideNav").css('-webkit-transform'))[4],
+			'pageWal' : parseToMatrix($(".pageWal").css('-webkit-transform'))[4]
+		};
+	}).on('touchend', function(){
+		$(".sideNav").removeClass('no-transition');
+		$(".pageWal").removeClass('no-transition');
+	});
+	
+	$("body").touchwipe({
+		wipeMoving: function(dx, dy) {
+				$(".sideNav").addClass('no-transition');
+				$(".pageWal").addClass('no-transition');
+			
+				var sideNavT3DX= initTranslate3dX.sideNav - dx*0.7;
+				var pageWalT3DX = initTranslate3dX.pageWal - dx*0.7;
+				
+				if(sideNavT3DX >= 0 || pageWalT3DX <= 0) {
+					return;
+				}
+				
+				$(".sideNav").css('transform', 'translate3d(' + sideNavT3DX + 'px, 0px, 0px)');
+				$(".sideNav").css('-webkit-transform', 'translate3d(' + sideNavT3DX + 'px, 0px, 0px)');
+				
+				$(".pageWal").css('transform', 'translate3d('+ pageWalT3DX + 'px, 0px, 0px)');
+				$(".pageWal").css('-webkit-transform', 'translate3d(' +  pageWalT3DX + 'px, 0px, 0px)');
+		},
+		wipeLeft: function(){
+			hideSideNav();
+		},
+		wipeRight: function(){
+			showSideNav();
+		},
+	});
+	hideSideNav();
+	
+	//--焦点图
 	//
-	})
+});
